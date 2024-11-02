d1612e353e3b4b504cbd8399458724b07d97a4c9
Avadhut Tanugade <avdhuttanugade55@gmail.com>
2024-11-03 00:04:01 +0530

    chore: update the correct vus for performance tests

--- src/main/load_tests/post_short_url.js
+++ src/main/load_tests/post_short_url.js
@@ -1,31 +1,25 @@
 import http from 'k6/http'
 import {check, sleep} from 'k6'
 
-
-let vus = 10100
+const port = 8080
+const vus = 330
 
 function getRandomNumber(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-let randomNum = getRandomNumber(1, 6000);
-
-
 export let options = {
-    stages: [{duration: '10s', target: vus}, // ramp-up to 20 VUs over 30 seconds
-        {duration: '60s', target: vus},  // stay at 20 VUs for 1 minute
-        {duration: '10s', target: 0},  // ramp-down to 0 VUs over 30 seconds
+    stages: [{duration: '10s', target: vus},
+        {duration: '60s', target: vus},
+        {duration: '10s', target: 0},
     ], thresholds: {
-        http_req_duration: ['p(95)<1000'], // 95% of requests should be below 500ms
-        http_req_failed: ['rate<0.01'],   // error rate should be less than 1%
+        http_req_duration: ['p(95)<1000'],
+        http_req_failed: ['rate<0.01'],
     },
 };
-
-let port = 8080
-
 export default function () {
-    let random = Math.random();
-    let url = `http://0.0.0.0:${port}/shorten?url=https://hello.world/${random}`;
+    const randomNum = getRandomNumber(1, vus);
+    const url = `http://0.0.0.0:${port}/shorten?url=https://hello.world/${randomNum}`;
     let response = http.post(url);
 
     // Check that the response is successful
