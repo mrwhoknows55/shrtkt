aa8c4eec7147242c965ca0d1e0787f9403d9726e
Avadhut Tanugade <avdhuttanugade55@gmail.com>
2025-09-09 01:02:40 +0530

    feat: add /status health-check endpoint

--- src/main/kotlin/xyz/avdt/routes/General.kt
+++ src/main/kotlin/xyz/avdt/routes/General.kt
@@ -3,13 +3,25 @@ package xyz.avdt.routes
 import io.ktor.http.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
+import org.jetbrains.exposed.v1.jdbc.selectAll
+import org.jetbrains.exposed.v1.jdbc.transactions.transaction
+import xyz.avdt.entities.UserTable
 
 fun Routing.commonRoutes() {
     get("/") {
         call.respondText("Shrtkt: Url Shortener is running!")
     }
     get("/status") {
-        call.respondText("OK", status = HttpStatusCode.OK)
+        val count = transaction {
+            runCatching {
+                UserTable.selectAll().count()
+            }.getOrElse { -1L }
+        }
+        if (count >= 0L) {
+            call.respondText("OK", status = HttpStatusCode.OK)
+        } else {
+            call.respondText("Server Error", status = HttpStatusCode.InternalServerError)
+        }
     }
     get("/top-urls") {
         call.respondRedirect("/stats/top-urls")
