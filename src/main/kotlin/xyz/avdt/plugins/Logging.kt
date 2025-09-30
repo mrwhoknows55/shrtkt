ad53ef39bd73b57d6b659b5c3ab93b647e7c24b1
Avadhut Tanugade <avdhuttanugade55@gmail.com>
2025-09-30 22:41:36 +0530

    feat: add call logs into new db table

--- src/main/kotlin/xyz/avdt/plugins/Logging.kt
+++ src/main/kotlin/xyz/avdt/plugins/Logging.kt
@@ -3,6 +3,9 @@ package xyz.avdt.plugins
 import io.ktor.server.application.*
 import io.ktor.server.plugins.calllogging.*
 import io.ktor.server.request.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import org.jetbrains.exposed.v1.jdbc.insert
 import org.jetbrains.exposed.v1.jdbc.transactions.transaction
 import xyz.avdt.entities.ApiLogTable
@@ -18,20 +21,24 @@ fun Application.configureCallLogging() {
             val status = call.response.status()
             val httpMethod = call.request.httpMethod
             val userAgent = call.request.userAgent()
-            val ip = call.request.local.remoteAddress
-            val local = call.request.local
+            val forwardedFor = call.request.header("X-Forwarded-Fo")
+            val realIp = call.request.header("X-Real-IP")
+            println("forwardedFor: $forwardedFor | realIp: $realIp")
+            val ip = realIp ?: forwardedFor ?: call.request.local.remoteAddress
             val path = call.request.path()
             val logLine =
-                "Time: $ts, HTTP method: $httpMethod, Path: $path, Status: $status, IP Address: $ip, User agent: $userAgent, Local: $local"
+                "Time: $ts, HTTP method: $httpMethod, Path: $path, Status: $status, IP Address: $ip, User agent: $userAgent"
             val startTime = Clock.System.now()
-            transaction {
-                ApiLogTable.insert {
-                    it[ApiLogTable.createdAt] = currentLocalDateTime()
-                    it[ApiLogTable.httpMethod] = httpMethod.toString()
-                    it[ApiLogTable.path] = path
-                    it[ApiLogTable.status] = status?.value ?: -1
-                    it[ApiLogTable.ipAddress] = ip
-                    it[ApiLogTable.userAgent] = userAgent.toString()
+            CoroutineScope(Dispatchers.IO).launch {
+                transaction {
+                    ApiLogTable.insert {
+                        it[ApiLogTable.createdAt] = currentLocalDateTime()
+                        it[ApiLogTable.httpMethod] = httpMethod.toString()
+                        it[ApiLogTable.path] = path
+                        it[ApiLogTable.status] = status?.value ?: -1
+                        it[ApiLogTable.ipAddress] = ip
+                        it[ApiLogTable.userAgent] = userAgent.toString()
+                    }
                 }
             }
             val diff = Clock.System.now() - startTime
